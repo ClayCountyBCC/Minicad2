e974229f7e69ace093b7c1db43462e62a2bfa32f
Daniel McCartney <daniel.mccartney@claycountygov.com>
2020-02-13 15:54:40 -0500

    initial commit, create react app seems to be working.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Minicad2.Global" Language="C#" %>
---
+++ src/interfaces.tsx
@@ -0,0 +1,14 @@
+﻿import SimpleValue from './SimpleValue';
+
+export interface IState
+{
+  current_view: string;
+  map: any;
+  map_view: any;
+}
+
+export interface IAction
+{
+  type: string
+  payload: any
+}
